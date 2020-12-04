9eeb07c381d05adedc9e7b5733b9004b5d3b1bb6
Jack Goldsmith <jackgoldsmith4@gmail.com>
2020-12-04 12:26:46 -0500

    fix

--- exceptions/exception-handler.js
+++ exceptions/exception-handler.js
@@ -1,16 +1,8 @@
-const UnauthorizedRequestException = require("./unauthorized-request-exception"),
-    BadRequestException = require("./bad-request-exception"),
-    InternalErrorException = require("./internal-error-exception");
+const InternalErrorException = require("./internal-error-exception");
 
 exports.retrieveException = function (err) {
     if (err instanceof InternalErrorException) {
         console.error(`INTERNAL ERROR "${err.message}": ${err.e.stack}`);
-        return err.message;
-    } else if (err instanceof BadRequestException) {
-        console.error(`BAD REQUEST "${err.message}": ${err.e.stack}`);
-        return err.message;
-    } else if (err instanceof InternalErrorException) {
-        console.error(`UNAUTHORIZED REQUEST "${err.message}": ${err.e.stack}`);
         return err.message;
     } else {
         console.error(`Uncaught exception occurred: ${err.stack}`);
