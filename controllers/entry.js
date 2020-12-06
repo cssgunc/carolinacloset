48bed6ec764cac57a70974fdaa1f65378c08090b
Owen Stegall <ostegall@live.unc.edu>
2020-12-06 14:32:36 -0500

    check2

--- controllers/entry.js
+++ controllers/entry.js
@@ -9,7 +9,7 @@ const express = require("express"),
     userIsVolunteer = require("./util/auth.js").userIsVolunteer;
     userIsAdmin = require("./util/auth.js").userIsAdmin;
     // Edit: changed volunteer to Admin throughout
-    // Edit: changed barcode and desc to type throughou
+    // Edit: changed barcode and desc to type througho
    
 
 const MANUAL_UPDATE_SUCCESS_MESSAGE = "Item successfully updated!";
